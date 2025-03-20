c9983d71a9051798bb4783cdcff2f1cfdfde9263
WizzOFF <maher_off@proton.me>
2025-03-20 22:15:49 +0100

    storing_data_with_linked_lists.c: manage the list using function > delete an element

--- storing_data_with_linked_lists.c
+++ storing_data_with_linked_lists.c
@@ -44,3 +44,18 @@ void insertion(Liste *liste, int nvNombre)
     nouveau->suivant = liste->premier;
     liste->premier = nouveau;
 }
+
+void suppression(Liste *liste)
+{
+    if (liste == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if (liste->premier != NULL)
+    {
+        Element *aSupprimer = liste->premier;
+        liste->premier = liste->premier->suivant;
+        free(aSupprimer);
+    }
+}
